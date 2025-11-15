07ea592cb79e2d408a3391701fb6d7483e1a9cdd
sadafparveen22 <sadafansari2212@gmail.com>
2025-11-15 18:27:52 +0530

    subscription changed (#1)
    Good code

--- Enviorment/Dev/provider.tf
+++ Enviorment/Dev/provider.tf
@@ -6,15 +6,15 @@ terraform {
     }
   }
   backend "azurerm" {
-    storage_account_name = "rahulstg3"
-    resource_group_name = "rahulrg"
-    container_name = "hello"
+    storage_account_name = "tokri"
+    resource_group_name = "sabke-liye-rg"
+    container_name = "tijori"
     key  = "dev.terraform.tfstate"    
   }
 }
 
 provider "azurerm" {
     features{}
-    subscription_id = "4708a5a8-f77a-4e4c-a78b-41d72efd5e7e"
+    subscription_id = "e5efa2cd-02a5-4c7b-be99-6e2ae6d5feac"
   
 }
